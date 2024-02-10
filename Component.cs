8dd1ad2e3c2a088d0cd3d465eda042217737e544
dvize <dvize404@gmail.com>
2024-02-10 14:13:59 -0800

    Add project files.

---
+++ Component.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BepInEx.Logging;
+using Comfort.Common;
+using EFT;
+using EFT.Interactive;
+using GTFO;
+using UnityEngine;
+
+public class GTFOComponent : MonoBehaviour
+{
+    private static GameWorld gameWorld;
+    private List<ExfiltrationPoint> enabledExfiltrationPoints = new List<ExfiltrationPoint>();
+    private Player player;
+    private bool displayActive;
+    private GUIStyle style;
+    private Vector3[] extractPositions; 
+    private string[] extractNames; 
+    private float[] extractDistances;
+    private Vector3 screenPosition;
+
+    protected static ManualLogSource Logger
+    {
+        get; private set;
+    }
+
+    private Dictionary<string, Vector3> labelsAndPositions = new Dictionary<string, Vector3>();
+
+
+    public GTFOComponent()
+    {
+        if (Logger == null)
+        {
+            Logger = BepInEx.Logging.Logger.CreateLogSource(nameof(GTFOComponent));
+        }
+    }
+
+    private void Start()
+    {
+        player = gameWorld.MainPlayer;
+        enabledExfiltrationPoints.Clear();
+
+        SetupInitialExtracts();
+
+        extractDistances = new float[enabledExfiltrationPoints.Count];
+        extractPositions = new Vector3[enabledExfiltrationPoints.Count];
+        extractNames = new string[enabledExfiltrationPoints.Count];
+    }
+
+    private void SetupInitialExtracts()
+    {
+        foreach (ExfiltrationPoint exfiltrationPoint in gameWorld.ExfiltrationController.ExfiltrationPoints)
+        {
+            if (exfiltrationPoint.isActiveAndEnabled)
+            {
+                enabledExfiltrationPoints.Add(exfiltrationPoint);
+            }
+        }
+
+        Logger.LogWarning("Enabled Exfiltration Points: " + enabledExfiltrationPoints.Count);
+    }
+
+    private void Update()
+    {
+        if (GTFOPlugin.enabledPlugin.Value == false)
+        {
+            return;
+        }
+
+        if (GTFOPlugin.keyboardShortcut.Value.IsDown() && !displayActive)
+        {
+            ToggleExtractionPointsDisplay(true);
+        }
+
+        UpdateLabels();
+    }
+
+    private void ToggleExtractionPointsDisplay(bool display)
+    {
+        displayActive = display;
+        if (display)
+        {
+            StartCoroutine(HideExtractionPointsAfterDelay(GTFOPlugin.displayTime.Value));
+        }
+        else
+        {
+            HideExtractionPoints();
+        }
+    }
+
+    private IEnumerator HideExtractionPointsAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        HideExtractionPoints();
+    }
+
+    private void HideExtractionPoints()
+    {
+        displayActive = false;
+    }
+
+    private void UpdateLabels()
+    {
+        for (int i = 0; i < enabledExfiltrationPoints.Count; i++) // Loop over enabledExfiltrationPoints directly
+        {
+            extractPositions[i] = enabledExfiltrationPoints[i].transform.position;
+            extractNames[i] = enabledExfiltrationPoints[i].name;
+            extractDistances[i] = Vector3.Distance(extractPositions[i], player.Position);
+        }
+    }
+
+
+    private void OnGUI()
+    {
+        if (displayActive)
+        {
+            DrawLabels();
+        }
+    }
+
+    private void DrawLabels()
+    {
+        if(style is null)
+        {
+            style = new GUIStyle();
+            style.normal.textColor = Color.white;
+            style.alignment = TextAnchor.MiddleCenter;
+        }
+        
+        for (int i = 0; i < extractPositions.Length; i++)
+        {
+            screenPosition = Camera.main.WorldToScreenPoint(extractPositions[i]);
+
+            string label = $"Extract Name: {extractNames[i]}\nDistance: {extractDistances[i]:F2} meters";
+            GUI.Label(new Rect(screenPosition.x, Screen.height - screenPosition.y, 200, 50), label, style); 
+        }
+    }
+
+    public static void Enable()
+    {
+        if (Singleton<IBotGame>.Instantiated)
+        {
+            gameWorld = Singleton<GameWorld>.Instance;
+            gameWorld.GetOrAddComponent<GTFOComponent>();
+
+            Logger.LogDebug("GTFO Enabled");
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,61 @@
+﻿using System.Reflection;
+using Aki.Reflection.Patching;
+using BepInEx;
+using BepInEx.Configuration;
+using EFT;
+using UnityEngine;
+
+namespace GTFO
+{
+    [BepInPlugin("com.dvize.GTFO", "dvize.GTFO", "1.0.0")]
+    public class GTFOPlugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> enabledPlugin;
+        public static ConfigEntry<float> distanceLimit;
+        public static ConfigEntry<KeyboardShortcut> keyboardShortcut;
+        public static ConfigEntry<float> displayTime;
+
+        private void Awake()
+        {
+
+            enabledPlugin = Config.Bind(
+                "Main Settings",
+                "Enable",
+                true,
+                "Enable the plugin to show with extracts dialog");
+
+            distanceLimit = Config.Bind(
+                "Main Settings",
+                "Distance Limit",
+                300f,
+                "Show Extracts at a Maximum Distance of Up To");
+
+            keyboardShortcut = Config.Bind(
+                "Main Settings",
+                "Keyboard Shortcut",
+                new KeyboardShortcut(KeyCode.O),
+                "Toggle Extracts Dialog");
+
+            displayTime = Config.Bind(
+                "Main Settings",
+                "Display Time",
+                5f,
+                "Time to Display Extracts Dialog");
+
+            new NewGamePatch().Enable();
+        }
+
+    }
+
+    //re-initializes each new game
+    internal class NewGamePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() => typeof(GameWorld).GetMethod(nameof(GameWorld.OnGameStarted));
+
+        [PatchPrefix]
+        public static void PatchPrefix()
+        {
+            GTFOComponent.Enable();
+        }
+    }
+}
