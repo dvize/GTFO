9a9fea352e2b238597a6e58b3e65cf783a0adb9e
dvize <dvize404@gmail.com>
2024-04-14 23:19:50 -0700

    added in-game check for the TryNotifyConditionChanged Patch as it was causing issues with turning in quests.

--- Quest/TryNotifyConditionChangedPatch.cs
+++ Quest/TryNotifyConditionChangedPatch.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using Aki.Reflection.Patching;
+using Comfort.Common;
+using EFT;
 using HarmonyLib;
+using UnityEngine;
 
 namespace GTFO
 {
@@ -15,7 +18,35 @@ namespace GTFO
         [PatchPostfix]
         public static void Postfix(ref GClass1249 quest)
         {
-            GTFOComponent.questManager.OnQuestsChanged(quest);
+            if (Singleton<GameWorld>.Instance == null)
+            {
+                Debug.LogError("TryNotifyConditionChanged Postfix: GameWorld instance is null.");
+                return;
+            }
+
+            if (Singleton<GameWorld>.Instance.TryGetComponent<GTFOComponent>(out GTFOComponent gtfo))
+            {
+                if (gtfo != null && quest != null)
+                {
+                    if (GTFOComponent.questManager != null)
+                    {
+                        GTFOComponent.questManager.OnQuestsChanged(quest);
+                    }
+                    else
+                    {
+                        Debug.LogError("TryNotifyConditionChanged Postfix: QuestManager is null within GTFOComponent.");
+                    }
+                }
+                else
+                {
+                    Debug.LogError($"TryNotifyConditionChanged Postfix: Either 'gtfo' is null ({gtfo == null}) or 'quest' is null ({quest == null}).");
+                }
+            }
+            else
+            {
+                Debug.LogError("TryNotifyConditionChanged Postfix: Failed to retrieve GTFOComponent from GameWorld.");
+            }
+
 
         }
     }
